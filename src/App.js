662310503857e61e42f9be544c9e4f249eab584a
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2023-03-17 17:01:33 +0530

    dark mode and light mode

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 import './App.css';
+import About from './components/About';
 
 function App() {
-  return ( <><Navbar title ="BuildApp"/>
-  <div className="container"><TextForm heading="Enter the Text"/></div>
+  return ( 
+  <>
+  <Navbar title ="BuildApp"/>
+  <About/>
+  {/* <div className="container"><TextForm heading="Enter the Text"/></div> */}
 </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TextForm(props) {
+  const [text, setText] = useState("Enter Text Here");
+
+  function handleOnChange(event) {
+    // console.log("On change")
+    setText(event.target.value)
+  }
+
+  function handleUpperClick() {
+    let newText = text.toUpperCase();
+    let paraWords = text.split(" ")
+    console.log(paraWords)
+    setText(newText);
+
+    // console.log("Button Of Uppercase was Clicked")
+  }
   return (
-    <div>
+    <>
+    <div className = "container">
   <div className="mb-3">
     <label htmlFor="exampleFormControlTextarea1" className="form-label">{props.heading}</label>
-    <textarea className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+    <textarea onChange={handleOnChange} value={text} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
   </div>
-  <button className="btn btn-primary">Convert to UpperCase</button></div>
+  <button className="btn btn-primary" onClick = {handleUpperClick}>Convert to UpperCase</button></div>
+  
+  <div className = "container">
+    <h2>Paragraph Summary</h2>
+    <p>Words in Paragraph: {text.split(" ").length} , Characters in Paragraph: {text.length}</p>
+  </div>
+  </>
   )
 }
