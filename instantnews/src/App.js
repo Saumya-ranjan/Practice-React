8714aad9e29e41f26b5db9597347012d301c9f75
Saumyaranjan <saumyaranjan.sp20@gmail.com>
2023-03-24 23:31:41 +0530

    starting with notex

---
+++ instantnews/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>Hello My First Class Based component</div>
+    )
+  }
+}
+
